290b7080fe385f2ec519a97a96e3fe2b37a7b244
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-11-30 20:07:06 +0600

    add footer

--- resources/views/blocks/middle.blade.php
+++ resources/views/blocks/middle.blade.php
@@ -1,9 +1,44 @@
-<div>
-    <p class="h1 text-white">Summer Tyres</p>
-    <p class="text-white">limited time free shipping</p>
-
-    <p class="text-white">from $99.99</p>
-
-    <p class="text-white">Find your tire</p>
-    <p class="text-white">The fastest way to the perfect tire</p>
+<div class="container">
+    <div class="">
+        <div class="">
+            <div id="owl-text" class="owl-carousel owl-theme">
+                <div class="item text-white">
+                    <p class="h1 text-white">Summer Tyres</p>
+                    <p class="text-white">limited time free shipping</p>
+                    <p class="text-white">from $99.99</p>
+                </div>
+                <div class="item text-white">
+                    <p class="h1 text-white">Summer Tyres</p>
+                    <p class="text-white">limited time free shipping</p>
+                    <p class="text-white">from $99.99</p>
+                </div>
+                <div class="item text-white">
+                    <p class="h1 text-white">Summer Tyres</p>
+                    <p class="text-white">limited time free shipping</p>
+                    <p class="text-white">from $99.99</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <p class="text-white">Find your tire</p>
+        <p class="text-white">The fastest way to the perfect tire</p>
+    </div>
 </div>
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('https://raw.githubusercontent.com/daneden/animate.css/master/animate.css') }}">
+@endpush
+@push('scripts')
+    <script>
+        $('#owl-text').owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 10,
+            mouseDrag: false,
+            touchDrag: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            dots: true,
+        });
+    </script>
+@endpush
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,12 @@
     <section style="background: url({{ asset('img/bg_emmo.png') }}) no-repeat; background-size: cover;height: 1000px">
         @include('blocks.header')
         @include('blocks.middle')
+
     </section>
     <section style="background: url({{ asset('img/bg.png') }}) no-repeat; background-size: cover;height: 2000px;padding: 0;margin: 0">
 
     </section>
     <section style="background: url({{ asset('img/bg_footer.png') }}) no-repeat; background-size: cover;height: 1700px;">
-
+        @include('blocks.footer')
     </section>
 @endsection
