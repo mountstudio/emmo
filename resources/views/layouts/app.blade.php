19be70a4ef4ed96455994383ec8e12fff4769db7
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-12-03 14:54:31 +0600

    fx header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-
+    <script src="{{ asset('js/fontawesome.js') }}" crossorigin="anonymous"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
