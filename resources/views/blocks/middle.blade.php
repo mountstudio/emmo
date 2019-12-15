9b2af8ce17d689b34f5f9cf595288f81c216cef3
Uki7991 <tilek.kubanov@gmail.com>
2019-12-15 14:29:49 +0600

    Update middle.blade.php

--- resources/views/blocks/middle.blade.php
+++ resources/views/blocks/middle.blade.php
@@ -1,37 +1,37 @@
 <div class="main-container">
     <div class="row">
-        <div class="col-12 col-md-7 pt-3">
-            <div id="owl-text" class="owl-carousel owl-theme">
-                <div class="item text-white">
-                    <h1 class="display-3 text-white text_for_welcome m-0">Summer Tyres</h1>
-                    <p class="text-white text_for_welcome h5"> limited time free shipping</p>
-                    <hr class="middle_hr">
-                    <div class=" d-flex col-5">
-                        <p><span class="text-white text_for_welcome pr-4 h5">from</span><span class="text_for_price h1">$99.99</span>
-                        </p>
-                    </div>
-                </div>
-                <div class="item text-white">
-                    <h1 class="display-3 text-white text_for_welcome m-0">Summer Tyres</h1>
-                    <p class="text-white text_for_welcome h5"> limited time free shipping</p>
-                    <hr class="middle_hr">
-                    <div class=" d-flex col-5">
-                        <p><span class="text-white text_for_welcome pr-4 h5">from</span><span class="text_for_price h1">$99.99</span>
-                        </p>
-                    </div>
-                </div>
-                <div class="item text-white">
-                    <h1 class="display-3 text-white text_for_welcome m-0">Summer Tyres</h1>
-                    <p class="text-white text_for_welcome h5"> limited time free shipping</p>
-                    <hr class="middle_hr">
-                    <div class=" d-flex col-5">
-                        <p><span class="text-white text_for_welcome pr-4 h5">from</span><span class="text_for_price h1">$99.99</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-12 col-md-5 pt-3 text-white">
+{{--        <div class="col-12 col-md-7 pt-3">--}}
+{{--            <div id="owl-text" class="owl-carousel owl-theme">--}}
+{{--                <div class="item text-white">--}}
+{{--                    <h1 class="display-3 text-white text_for_welcome m-0">Summer Tyres</h1>--}}
+{{--                    <p class="text-white text_for_welcome h5"> limited time free shipping</p>--}}
+{{--                    <hr class="middle_hr">--}}
+{{--                    <div class=" d-flex col-5">--}}
+{{--                        <p><span class="text-white text_for_welcome pr-4 h5">from</span><span class="text_for_price h1">$99.99</span>--}}
+{{--                        </p>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="item text-white">--}}
+{{--                    <h1 class="display-3 text-white text_for_welcome m-0">Summer Tyres</h1>--}}
+{{--                    <p class="text-white text_for_welcome h5"> limited time free shipping</p>--}}
+{{--                    <hr class="middle_hr">--}}
+{{--                    <div class=" d-flex col-5">--}}
+{{--                        <p><span class="text-white text_for_welcome pr-4 h5">from</span><span class="text_for_price h1">$99.99</span>--}}
+{{--                        </p>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="item text-white">--}}
+{{--                    <h1 class="display-3 text-white text_for_welcome m-0">Summer Tyres</h1>--}}
+{{--                    <p class="text-white text_for_welcome h5"> limited time free shipping</p>--}}
+{{--                    <hr class="middle_hr">--}}
+{{--                    <div class=" d-flex col-5">--}}
+{{--                        <p><span class="text-white text_for_welcome pr-4 h5">from</span><span class="text_for_price h1">$99.99</span>--}}
+{{--                        </p>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+        <div class="col-12 col-md-6 pt-3 text-white">
             <form class="text-left border border-light p-5 form-bg" action="#!">
                 <p class="h4 mb-3">Search by tires brand</p>
                 <label>Brand</label>
@@ -90,9 +90,11 @@
                     <a href="" class="text-white btn-search">Search</a>
                 </div>
             </form>
-            <div class="pt-4">
+        </div>
+        <div class="col-12 col-md-6 pt-3 text-white">
+
                 <form class="text-left border border-light p-5 form-bg text-white" action="#!">
-                    <p class="h4 mb-4">Leave a request</p>
+                    <p class="h4 mb-3">Find me a tire</p>
                     <label>Tire size</label>
                     <div class="form-row">
                         <div class="col-12 col-md-4">
@@ -128,10 +130,9 @@
                     <label>Telephone number</label>
                     <input type="phone" id=""  class="form-control mb-4 arrow-for-select text-white" >
                     <div>
-                        <a href="" class="text-white btn-leave">Leave</a>
+                        <a href="" class="text-white btn-leave">Find!</a>
                     </div>
                 </form>
-            </div>
         </div>
     </div>
 </div>
